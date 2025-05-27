13155ade3fb171590c6198ae5153ee014d93fbb2
catalinbroinas <catalinbroinas@yahoo.com>
2025-05-27 12:24:02 +0300

    feat(React): mount React into #stadiums

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,21 @@ import '../scss/main.scss';
 
 import { Ripple, Dropdown, Collapse, initMDB } from 'mdb-ui-kit/js/mdb.es.min.js';
 
+import React, { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+
 function MainDomManager() {
+    const renderStadiums = () => {
+        const stadiumsElement = document.querySelector('#stadiums');
+        if (!stadiumsElement) return;
+
+        const root = createRoot(stadiumsElement);
+        root.render(React.createElement('h1', null, 'React work!'));
+    };
+
     const initApp = () => {
         initMDB({ Ripple, Dropdown, Collapse });
+        renderStadiums();
     };
 
     return { initApp };
