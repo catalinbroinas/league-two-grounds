26a1f6650c02af737f93e76ddbba15a8ea673325
catalinbroinas <catalinbroinas@yahoo.com>
2025-05-30 18:13:02 +0300

    refactor(StadiumList): render stadiums from data
    - Use StadiumCard for each stadium
    - Pass stadium and team as props

--- src/components/StadiumList.jsx
+++ src/components/StadiumList.jsx
@@ -1,11 +1,14 @@
 import StadiumCard from "./StadiumCard";
+import stadiums from "../data/stadiums";
 
 function StadiumList() {
     return (
         <div className="row row-cols-xxl-4 row-cols-lg-3 row-cols-sm-2 row-cols-1 g-xl-5 g-4">
-            <div className="col">
-                <StadiumCard />
-            </div>
+            {stadiums.map(item =>
+                <div key={item.id} className="col">
+                    <StadiumCard stadium={item.stadium} team={item.team} />
+                </div>
+            )}
         </div>
     );
 }
