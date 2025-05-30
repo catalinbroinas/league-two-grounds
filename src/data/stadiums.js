f4dfc07980c4575086b4450842605bcb37e7c63a
catalinbroinas <catalinbroinas@yahoo.com>
2025-05-30 17:40:07 +0300

    feat(stadiums.js): add stadiums object
    - Include 2 stadiums: MK Stadium, Valley Parade

---
+++ src/data/stadiums.js
@@ -0,0 +1,36 @@
+import mkStadium from '../assets/img/MKStadium.jpg';
+import mkLogo from '../assets/img/mkDons.avif';
+
+import valleyParade from '../assets/img/valleyParade.jpg';
+import bcLogo from '../assets/img/BradfordCity.png';
+
+const stadiums = [
+    {
+        id: crypto.randomUUID(),
+        stadium: {
+            name: 'MK Stadium',
+            image: mkStadium,
+            buildYear: 2010,
+            capacity: 30000
+        },
+        team: {
+            name: 'MK Dons',
+            image: mkLogo
+        }
+    },
+    {
+        id: crypto.randomUUID(),
+        stadium: {
+            name: 'Valley Parade',
+            image: valleyParade,
+            buildYear: 1886,
+            capacity: 25000
+        },
+        team: {
+            name: 'Bradford City',
+            image: bcLogo
+        }
+    }
+];
+
+export default stadiums;
