55e4ef34fb52f7eb8b035331d1a1b54b3000b961
catalinbroinas <catalinbroinas@yahoo.com>
2025-06-05 20:20:09 +0300

    feat(images.js): add 13 club objects

--- src/data/images.js
+++ src/data/images.js
@@ -1,15 +1,81 @@
 // Stadiums
+import whamStadium from '../assets/img/stadiums/whamStadium.jpg';
+import cherryRed from '../assets/img/stadiums/cherryRedRecords2.jpg';
+import holkerStreet from '../assets/img/stadiums/holkerStreet.png';
 import valleyParade from '../assets/img/stadiums/valleyParade.jpg';
-import mkStadium from '../assets/img/stadiums/MKStadium.jpg';
+import hayesLane from '../assets/img/stadiums/hayesLane.jpg';
+import bruntonPark from '../assets/img/stadiums/bruntonPark.jpg';
+import completelySuzuki from '../assets/img/stadiums/completelySuzukiStadium.jpg';
+import smhGroup from '../assets/img/stadiums/smhGroup.jpg';
+import jobServe from '../assets/img/stadiums/jobserve.jpg';
+import mornFlake from '../assets/img/stadiums/mornflake.jpg';
+import ecoPower from '../assets/img/stadiums/ecoPower2.jpg';
+import highburyStadium from '../assets/img/stadiums/highbury.jpg';
+import mkStadium from '../assets/img/stadiums/stadiumMK1.jpg';
 
 // Teams
-import bcLogo from '../assets/img/teams/BradfordCity.png';
-import mkLogo from '../assets/img/teams/mkDons.avif';
+import accringtonLogo from '../assets/img/teams/accrington.png';
+import wimbledonLogo from '../assets/img/teams/afcW.webp';
+import barrowLogo from '../assets/img/teams/barrow.png';
+import bradfordLogo from '../assets/img/teams/BradfordCity.png';
+import bromleyLogo from '../assets/img/teams/bromley.png';
+import carlisleLogo from '../assets/img/teams/carlisle.png';
+import cheltenhamLogo from '../assets/img/teams/cheltanham.png';
+import chesterfieldLogo from '../assets/img/teams/chesterfield.png';
+import colchesterLogo from '../assets/img/teams/colchester.png';
+import creweLogo from '../assets/img/teams/crewe.png';
+import doncasterLogo from '../assets/img/teams/doncaster.png';
+import fleetwoodLogo from '../assets/img/teams/fletwood.png';
+import mkLogo from '../assets/img/teams/mk.png';
 
 const images = {
+    accringtonStanley: {
+        stadium: whamStadium,
+        team: accringtonLogo
+    },
+    afcWimbledon: {
+        stadium: cherryRed,
+        team: wimbledonLogo
+    },
+    barrow: {
+        stadium: holkerStreet,
+        team: barrowLogo
+    },
     bradfordCity: {
         stadium: valleyParade,
-        team: bcLogo
+        team: bradfordLogo
+    },
+    bromley: {
+        stadium: hayesLane,
+        team: bromleyLogo
+    },
+    carlisleUnited: {
+        stadium: bruntonPark,
+        team: carlisleLogo
+    },
+    cheltenhamTown: {
+        stadium: completelySuzuki,
+        team: cheltenhamLogo
+    },
+    chesterfield: {
+        stadium: smhGroup,
+        team: chesterfieldLogo
+    },
+    colchesterUnited: {
+        stadium: jobServe,
+        team: colchesterLogo
+    },
+    creweAlexandra: {
+        stadium: mornFlake,
+        team: creweLogo
+    },
+    doncasterRovers: {
+        stadium: ecoPower,
+        team: doncasterLogo
+    },
+    fleetwoodTown: {
+        stadium: highburyStadium,
+        team: fleetwoodLogo
     },
     mkDons: {
         stadium: mkStadium,
