5fa39d2fbc5314cf68dcb583fc78f1733e4fb20b
catalinbroinas <catalinbroinas@yahoo.com>
2025-08-21 18:52:14 +0300

    chore: add PurgeCSS

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+import purgecss from '@fullhuman/postcss-purgecss';
+
+const purgecssConfig = purgecss({
+    content: [
+        './index.html',
+        './src/**/*.{html,js,jsx,ts,tsx,vue}',
+    ],
+    safelist: [
+        /^modal/,
+        /^dropdown/,
+        /^collapse/,
+        /^show$/,
+        /^fade$/,
+        /^btn-/,
+        /^bg-/,
+        /^text-/,
+        /^ripple/,
+        /^tooltip/,
+        /^accordion/,
+        /^offcanvas/,
+        /^toast/,
+    ],
+});
+
+export default ({ env }) => ({
+    plugins: [
+        env === 'production' ? purgecssConfig : false,
+    ],
+});
